b8041204e5a756c1c02e5e5e2d21e0cd985deec5
MarcoFedele <marco.fedele@uniroma1.it>
2019-05-29 17:23:40 +0200

    Model creation completed for FN

--- Flavour/src/FroggattNielsen.cpp
+++ Flavour/src/FroggattNielsen.cpp
@@ -7,10 +7,57 @@
 
 #include "FroggattNielsen.h"
 
-const std::string FroggattNielsen::FroggattNielsenvars[NFroggattNielsenvars] = {"logma"};
+const std::string FroggattNielsen::FroggattNielsenvars[NFroggattNielsenvars] = {
+    "eps", "mtopEW",
+    "s12CKM", "s13CKM", "s23CKM", "deltaCKM",
+    "QL1", "QL2", "QL3",
+    "QRu1", "QRu2", "QRu3",
+    "QRd1", "QRd2", "QRd3",
+    "thL1", "thL2", "thL3", "phiL1", "phiL2", "phiL3", "phiL4", "phiL5",
+    "thRu1", "thRu2", "thRu3", "phiRu1", "phiRu2", "phiRu3", "phiRu4", "phiRu5",
+    "thRd1", "thRd2", "thRd3", "phiRd1", "phiRd2", "phiRd3", "phiRd4", "phiRd5"};
 
 FroggattNielsen::FroggattNielsen() : StandardModel() {   
-    ModelParamMap.insert(std::make_pair("logma", std::cref(ma)));
+    ModelParamMap.insert(std::make_pair("eps", std::cref(eps)));
+    ModelParamMap.insert(std::make_pair("mtopEW", std::cref(mtopEW)));
+    ModelParamMap.insert(std::make_pair("s12CKM", std::cref(s12CKM)));
+    ModelParamMap.insert(std::make_pair("s13CKM", std::cref(s13CKM)));
+    ModelParamMap.insert(std::make_pair("s23CKM", std::cref(s23CKM)));
+    ModelParamMap.insert(std::make_pair("deltaCKM", std::cref(deltaCKM)));
+    ModelParamMap.insert(std::make_pair("QL1", std::cref(QL1)));
+    ModelParamMap.insert(std::make_pair("QL2", std::cref(QL2)));
+    ModelParamMap.insert(std::make_pair("QL3", std::cref(QL3)));
+    ModelParamMap.insert(std::make_pair("QRu1", std::cref(QRu1)));
+    ModelParamMap.insert(std::make_pair("QRu2", std::cref(QRu2)));
+    ModelParamMap.insert(std::make_pair("QRu3", std::cref(QRu3)));
+    ModelParamMap.insert(std::make_pair("QRd1", std::cref(QRd1)));
+    ModelParamMap.insert(std::make_pair("QRd2", std::cref(QRd2)));
+    ModelParamMap.insert(std::make_pair("QRd3", std::cref(QRd3)));
+    ModelParamMap.insert(std::make_pair("thL1", std::cref(thL1)));
+    ModelParamMap.insert(std::make_pair("thL2", std::cref(thL2)));
+    ModelParamMap.insert(std::make_pair("thL3", std::cref(thL3)));
+    ModelParamMap.insert(std::make_pair("phiL1", std::cref(phiL1)));
+    ModelParamMap.insert(std::make_pair("phiL2", std::cref(phiL2)));
+    ModelParamMap.insert(std::make_pair("phiL3", std::cref(phiL3)));
+    ModelParamMap.insert(std::make_pair("phiL4", std::cref(phiL4)));
+    ModelParamMap.insert(std::make_pair("phiL5", std::cref(phiL5)));
+    ModelParamMap.insert(std::make_pair("thRu1", std::cref(thRu1)));
+    ModelParamMap.insert(std::make_pair("thRu2", std::cref(thRu2)));
+    ModelParamMap.insert(std::make_pair("thRu3", std::cref(thRu3)));
+    ModelParamMap.insert(std::make_pair("phiRu1", std::cref(phiRu1)));
+    ModelParamMap.insert(std::make_pair("phiRu2", std::cref(phiRu2)));
+    ModelParamMap.insert(std::make_pair("phiRu3", std::cref(phiRu3)));
+    ModelParamMap.insert(std::make_pair("phiRu4", std::cref(phiRu4)));
+    ModelParamMap.insert(std::make_pair("phiRu5", std::cref(phiRu5)));
+    ModelParamMap.insert(std::make_pair("thRd1", std::cref(thRd1)));
+    ModelParamMap.insert(std::make_pair("thRd2", std::cref(thRd2)));
+    ModelParamMap.insert(std::make_pair("thRd3", std::cref(thRd3)));
+    ModelParamMap.insert(std::make_pair("phiRd1", std::cref(phiRd1)));
+    ModelParamMap.insert(std::make_pair("phiRd2", std::cref(phiRd2)));
+    ModelParamMap.insert(std::make_pair("phiRd3", std::cref(phiRd3)));
+    ModelParamMap.insert(std::make_pair("phiRd4", std::cref(phiRd4)));
+    ModelParamMap.insert(std::make_pair("phiRd5", std::cref(phiRd5)));
+
 }
 
 FroggattNielsen::~FroggattNielsen(){
@@ -58,16 +105,129 @@ bool FroggattNielsen::PostUpdate()
 {
     if(!StandardModel::PostUpdate()) return (false);
 
-    /* Necessary for updating StandardModel parameters in StandardModelMatching,
-     * and Axions and Axions-derived parameters in AxionsMatching */
-    //LoopMediatorsM.getObj().updateLoopMediatorsParameters();
-
     return (true);
 }
 
 void FroggattNielsen::setParameter(const std::string name, const double& value){    
-    if(name.compare("logma") == 0) {
-        ma = pow(10., value);
+    if(name.compare("eps") == 0) {
+        eps = value;
+    }
+    else if(name.compare("mtopEW") == 0) {
+        mtopEW = value;
+    }
+    else if(name.compare("s12CKM") == 0) {
+        s12CKM = value;
+        c12CKM = sqrt(1. - s12CKM*s12CKM);
+    }
+    else if(name.compare("s13CKM") == 0) {
+        s13CKM = value;
+        c13CKM = sqrt(1. - s13CKM*s13CKM);
+    }
+    else if(name.compare("s23CKM") == 0) {
+        s23CKM = value;
+        c23CKM = sqrt(1. - s23CKM*s23CKM);
+    }
+    else if(name.compare("deltaCKM") == 0) {
+        deltaCKM = value;
+    }
+    else if(name.compare("QL1") == 0) {
+        QL1 = value;
+    }
+    else if(name.compare("QL2") == 0) {
+        QL2 = value;
+    }
+    else if(name.compare("QL3") == 0) {
+        QL3 = value;
+    }
+    else if(name.compare("QRu1") == 0) {
+        QRu1 = value;
+    }
+    else if(name.compare("QRu2") == 0) {
+        QRu2 = value;
+    }
+    else if(name.compare("QRu3") == 0) {
+        QRu3 = value;
+    }
+    else if(name.compare("QRd1") == 0) {
+        QRd1 = value;
+    }
+    else if(name.compare("QRd2") == 0) {
+        QRd2 = value;
+    }
+    else if(name.compare("QRd3") == 0) {
+        QRd3 = value;
+    }
+    else if(name.compare("thL1") == 0) {
+        thL1 = value;
+    }
+    else if(name.compare("thL2") == 0) {
+        thL2 = value;
+    }
+    else if(name.compare("thL3") == 0) {
+        thL3 = value;
+    }
+    else if(name.compare("phiL1") == 0) {
+        phiL1 = value;
+    }
+    else if(name.compare("phiL2") == 0) {
+        phiL2 = value;
+    }
+    else if(name.compare("phiL3") == 0) {
+        phiL3 = value;
+    }
+    else if(name.compare("phiL4") == 0) {
+        phiL4 = value;
+    }
+    else if(name.compare("phiL5") == 0) {
+        phiL5 = value;
+    }
+    else if(name.compare("thRu1") == 0) {
+        thRu1 = value;
+    }
+    else if(name.compare("thRu2") == 0) {
+        thRu2 = value;
+    }
+    else if(name.compare("thRu3") == 0) {
+        thRu3 = value;
+    }
+    else if(name.compare("phiRu1") == 0) {
+        phiRu1 = value;
+    }
+    else if(name.compare("phiRu2") == 0) {
+        phiRu2 = value;
+    }
+    else if(name.compare("phiRu3") == 0) {
+        phiRu3 = value;
+    }
+    else if(name.compare("phiRu4") == 0) {
+        phiRu4 = value;
+    }
+    else if(name.compare("phiRu5") == 0) {
+        phiRu5 = value;
+    }
+    else if(name.compare("thRd1") == 0) {
+        thRd1 = value;
+    }
+    else if(name.compare("thRd2") == 0) {
+        thRd2 = value;
+    }
+    else if(name.compare("thRd3") == 0) {
+        thRd3 = value;
+    }
+    else if(name.compare("phiRd1") == 0) {
+        phiRd1 = value;
+    }
+    else if(name.compare("phiRd2") == 0) {
+        phiRd2 = value;
+    }
+    else if(name.compare("phiRd3") == 0) {
+        phiRd3 = value;
+    }
+    else if(name.compare("phiRd4") == 0) {
+        phiRd4 = value;
+    }
+    else if(name.compare("phiRd5") == 0) {
+        phiRd5 = value;
     }
     else
         StandardModel::setParameter(name,value);
@@ -95,6 +255,163 @@ bool FroggattNielsen::setFlag(const std::string name, const bool value)
     return(res);
 }
 
+gslpp::matrix<double> FroggattNielsen::absCKM() const
+{   
+    gslpp::matrix<double> V(3,3);
+    
+    V.assign(0, 0, c12CKM*c13CKM);
+    V.assign(0, 1, s12CKM*c13CKM);
+    V.assign(0, 2, gslpp::complex(s13CKM, -deltaCKM, true).abs());
+
+    V.assign(1, 0, (-s12CKM * c23CKM - gslpp::complex(c12CKM * s23CKM*s13CKM, deltaCKM, true)).abs());
+    V.assign(1, 1, (c12CKM * c23CKM - gslpp::complex(s12CKM * s23CKM*s13CKM, deltaCKM, true)).abs());
+    V.assign(1, 2, s23CKM*c13CKM);
+
+    V.assign(2, 0, (s12CKM * s23CKM - gslpp::complex(c12CKM * c23CKM*s13CKM, deltaCKM, true)).abs());
+    V.assign(2, 1, (-c12CKM * s23CKM - gslpp::complex(s12CKM * c23CKM*s13CKM, deltaCKM, true)).abs());
+    V.assign(2, 2, c23CKM*c13CKM);  
+    
+    return V;
+}
+
+gslpp::matrix<double> FroggattNielsen::lambda_u() const
+{   
+    gslpp::matrix<double> lambdau(3,3,0);
+    
+    lambdau.assign(0, 0, getQuarks(QCD::UP).getMass()*sqrt(2.)/v());
+
+    lambdau.assign(1, 1, getQuarks(QCD::CHARM).getMass()*sqrt(2.)/v());
+
+    lambdau.assign(2, 2, getmtopEW()*sqrt(2.)/v());
+    
+    return lambdau;
+}
+
+gslpp::matrix<double> FroggattNielsen::lambda_d() const
+{   
+    gslpp::matrix<double> lambdad(3,3,0);
+    
+    lambdad.assign(0, 0, getQuarks(QCD::DOWN).getMass()*sqrt(2.)/v());
+
+    lambdad.assign(1, 1, getQuarks(QCD::STRANGE).getMass()*sqrt(2.)/v());
+
+    lambdad.assign(2, 2, getQuarks(QCD::BOTTOM).getMass()*sqrt(2.)/v());
+    
+    lambdad = absCKM()*lambdad;
+    
+    return lambdad;
+}
+
+gslpp::matrix<double> FroggattNielsen::create_V(double th1, double th2, double th3, double phi1, double phi2, double phi3, double phi4, double phi5) const
+{   
+    double c1 = cos(th1);
+    double s1 = sin(th1);
+    double c2 = cos(th2);
+    double s2 = sin(th2);
+    double c3 = cos(th3);
+    double s3 = sin(th3);
+    
+    gslpp::matrix<double> V(3,3,0);
+    
+    V.assign(0, 0, gslpp::complex(c1*c2, phi1, true).abs());
+    V.assign(0, 1, gslpp::complex(s1, phi3, true).abs());
+    V.assign(0, 2, gslpp::complex(c1*s2, phi4, true).abs());
+    
+    V.assign(1, 0, (gslpp::complex(s2*s3, -phi4-phi5, true)-gslpp::complex(c2*c3*s1, phi1+phi2-phi3, true)).abs());
+    V.assign(1, 1, gslpp::complex(c1*c3, phi2, true).abs());
+    V.assign(1, 2, (gslpp::complex(-c2*s3, -phi1-phi5, true)-gslpp::complex(c3*s1*s2, phi2-phi3+phi4, true)).abs());
+    
+    V.assign(2, 0, (-gslpp::complex(c3*s2, -phi2-phi4, true)-gslpp::complex(c2*s1*s3, phi1-phi3+phi5, true)).abs());
+    V.assign(2, 1, gslpp::complex(c1*s3, phi5, true).abs());
+    V.assign(2, 2, (gslpp::complex(c2*c3, -phi1-phi2, true)-gslpp::complex(s1*s2*s3, -phi3+phi4+phi5, true)).abs());
+    
+    return V;
+}
+
+gslpp::matrix<double> FroggattNielsen::y_u() const
+{   
+    gslpp::matrix<double> VL(3,3,0);
+    gslpp::matrix<double> VRu(3,3,0);
+    
+    VL = create_V(thL1, thL2, thL3, phiL1, phiL2, phiL3, phiL4, phiL5);
+    VRu = create_V(thRu1, thRu2, thRu3, phiRu1, phiRu2, phiRu3, phiRu4, phiRu5);
+    
+    gslpp::matrix<double> yu(3,3,0);
+    
+    yu = VL.transpose()*lambda_u()*VRu;
+    
+    return yu;
+}
+
+gslpp::matrix<double> FroggattNielsen::y_d() const
+{   
+    gslpp::matrix<double> VL(3,3,0);
+    gslpp::matrix<double> VRd(3,3,0);
+    
+    VL = create_V(thL1, thL2, thL3, phiL1, phiL2, phiL3, phiL4, phiL5);
+    VRd = create_V(thRd1, thRd2, thRd3, phiRd1, phiRd2, phiRd3, phiRd4, phiRd5);
+    
+    gslpp::matrix<double> yd(3,3,0);
+    
+    yd = VL.transpose()*lambda_d()*VRd;
+    
+    return yd;
+}
+
+gslpp::matrix<double> FroggattNielsen::F_u() const
+{   
+    double eps = geteps();
+    double QL1 = getQL1();
+    double QL2 = getQL2();
+    double QL3 = getQL3();
+    double QRu1 = getQRu1();
+    double QRu2 = getQRu2();
+    double QRu3 = getQRu3();
+    
+    gslpp::matrix<double> Fu(3,3,0);
+    
+    Fu.assign(0, 0, pow(eps, QL1 - QRu1));
+    Fu.assign(0, 1, pow(eps, QL1 - QRu2));
+    Fu.assign(0, 2, pow(eps, QL1 - QRu3));
+    
+    Fu.assign(1, 0, pow(eps, QL2 - QRu1));
+    Fu.assign(1, 1, pow(eps, QL2 - QRu2));
+    Fu.assign(1, 2, pow(eps, QL2 - QRu3));
+    
+    Fu.assign(2, 0, pow(eps, QL3 - QRu1));
+    Fu.assign(2, 1, pow(eps, QL3 - QRu2));
+    Fu.assign(2, 2, pow(eps, QL3 - QRu3));
+    
+    return Fu;
+}
+
+gslpp::matrix<double> FroggattNielsen::F_d() const
+{   
+    double eps = geteps();
+    double QL1 = getQL1();
+    double QL2 = getQL2();
+    double QL3 = getQL3();
+    double QRd1 = getQRd1();
+    double QRd2 = getQRd2();
+    double QRd3 = getQRd3();
+    
+    gslpp::matrix<double> Fd(3,3,0);
+    
+    Fd.assign(0, 0, pow(eps, QL1 - QRd1));
+    Fd.assign(0, 1, pow(eps, QL1 - QRd2));
+    Fd.assign(0, 2, pow(eps, QL1 - QRd3));
+    
+    Fd.assign(1, 0, pow(eps, QL2 - QRd1));
+    Fd.assign(1, 1, pow(eps, QL2 - QRd2));
+    Fd.assign(1, 2, pow(eps, QL2 - QRd3));
+    
+    Fd.assign(2, 0, pow(eps, QL3 - QRd1));
+    Fd.assign(2, 1, pow(eps, QL3 - QRd2));
+    Fd.assign(2, 2, pow(eps, QL3 - QRd3));
+    
+    return Fd;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Observables
@@ -111,6 +428,31 @@ test::~test()
 
 double test::computeThValue()
 {
-    return myFroggattNielsen->getma();
+   /* std::cout << "md : " << SM.getQuarks(QCD::DOWN).getMass() << std::endl;
+    std::cout << "mu : " << SM.getQuarks(QCD::UP).getMass() << std::endl;
+    std::cout << "ms : " << SM.getQuarks(QCD::STRANGE).getMass() << std::endl;
+    std::cout << "mc : " << SM.getQuarks(QCD::CHARM).getMass() << std::endl;
+    std::cout << "mb : " << SM.getQuarks(QCD::BOTTOM).getMass() << std::endl;
+    std::cout << "mt : " << myFroggattNielsen->getmtopEW() << std::endl << std::endl;
+    std::cout << "vev : " << SM.v()/sqrt(2.) << std::endl << std::endl;
+    std::cout << "ckm : " << myFroggattNielsen->absCKM() << std::endl << std::endl;
+    std::cout << "QL1 : " << myFroggattNielsen->getQL1() << std::endl;
+    std::cout << "QL2 : " << myFroggattNielsen->getQL2() << std::endl;
+    std::cout << "QL3 : " << myFroggattNielsen->getQL3() << std::endl;
+    std::cout << "QRu1 : " << myFroggattNielsen->getQRu1() << std::endl;
+    std::cout << "QRu2 : " << myFroggattNielsen->getQRu2() << std::endl;
+    std::cout << "QRu3 : " << myFroggattNielsen->getQRu3() << std::endl;
+    std::cout << "QRd1 : " << myFroggattNielsen->getQRd1() << std::endl;
+    std::cout << "QRd2 : " << myFroggattNielsen->getQRd2() << std::endl;
+    std::cout << "QRd3 : " << myFroggattNielsen->getQRd3() << std::endl << std::endl;*/
+    
+    std::cout << "lambda_u : " << myFroggattNielsen->lambda_u() << std::endl;
+    std::cout << "y_u : " << myFroggattNielsen->y_u() << std::endl;
+    std::cout << "F_u : " << myFroggattNielsen->F_u() << std::endl;
+    std::cout << "lambda_d : " << myFroggattNielsen->lambda_d() << std::endl;
+    std::cout << "y_d : " << myFroggattNielsen->y_d() << std::endl;
+    std::cout << "F_d : " << myFroggattNielsen->F_d() << std::endl << std::endl;
+    
+    return myFroggattNielsen->geteps();
 
 }
--- Flavour/src/FroggattNielsen.h
+++ Flavour/src/FroggattNielsen.h
@@ -19,7 +19,7 @@
 class FroggattNielsen: public StandardModel {
 public:
 
-    static const int NFroggattNielsenvars = 1;
+    static const int NFroggattNielsenvars = 39;
 
     static const std::string FroggattNielsenvars[NFroggattNielsenvars];
     
@@ -54,11 +54,391 @@ public:
     
     /**
      *
-     * @return \f$ m_a $\f
+     * @return \f$ \epsilon $\f
      */
-    double getma() const {
-        return ma;
+    double geteps() const {
+        return eps;
     }
+    
+    /**
+     *
+     * @return \f$ m_t $\f
+     */
+    double getmtopEW() const {
+        return mtopEW;
+    }
+    
+    /**
+     *
+     * @return \f$ s_12^{CKM} $\f
+     */
+    double gets12CKM() const {
+        return s12CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ c_12^{CKM} $\f
+     */
+    double getc12CKM() const {
+        return c12CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ s_13^{CKM} $\f
+     */
+    double gets13CKM() const {
+        return s13CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ c_13^{CKM} $\f
+     */
+    double getc13CKM() const {
+        return c13CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ s_23^{CKM} $\f
+     */
+    double gets23CKM() const {
+        return s23CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ c_23^{CKM} $\f
+     */
+    double getc23CKM() const {
+        return c23CKM;
+    }
+    
+    /**
+     *
+     * @return \f$ \delta_{CKM} $\f
+     */
+    double getdeltaCKM() const {
+        return deltaCKM;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{L1} $\f
+     */
+    double getQL1() const {
+        return QL1;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{L2} $\f
+     */
+    double getQL2() const {
+        return QL2;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{L3} $\f
+     */
+    double getQL3() const {
+        return QL3;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Ru1} $\f
+     */
+    double getQRu1() const {
+        return QRu1;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Ru2} $\f
+     */
+    double getQRu2() const {
+        return QRu2;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Ru3} $\f
+     */
+    double getQRu3() const {
+        return QRu3;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Rd1} $\f
+     */
+    double getQRd1() const {
+        return QRd1;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Rd2} $\f
+     */
+    double getQRd2() const {
+        return QRd2;
+    }
+    
+    /**
+     *
+     * @return \f$ Q_{Rd3} $\f
+     */
+    double getQRd3() const {
+        return QRd3;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{L1} $\f
+     */
+    double getthL1() const {
+        return thL1;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{L2} $\f
+     */
+    double getthL2() const {
+        return thL2;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{L3} $\f
+     */
+    double getthL3() const {
+        return thL3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{L1} $\f
+     */
+    double getphiL1() const {
+        return phiL1;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{L2} $\f
+     */
+    double getphiL2() const {
+        return phiL2;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{L3} $\f
+     */
+    double getphiL3() const {
+        return phiL3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{L4} $\f
+     */
+    double getphiL4() const {
+        return phiL4;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{L5} $\f
+     */
+    double getphiL5() const {
+        return phiL5;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Ru1} $\f
+     */
+    double getthRu1() const {
+        return thRu1;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Ru2} $\f
+     */
+    double getthRu2() const {
+        return thRu2;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Ru3} $\f
+     */
+    double getthRu3() const {
+        return thRu3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Ru1} $\f
+     */
+    double getphiRu1() const {
+        return phiRu1;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Ru2} $\f
+     */
+    double getphiRu2() const {
+        return phiRu2;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Ru3} $\f
+     */
+    double getphiRu3() const {
+        return phiRu3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Ru4} $\f
+     */
+    double getphiRu4() const {
+        return phiRu4;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Ru5} $\f
+     */
+    double getphiRu5() const {
+        return phiRu5;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Rd1} $\f
+     */
+    double getthRd1() const {
+        return thRd1;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Rd2} $\f
+     */
+    double getthRd2() const {
+        return thRd2;
+    }
+    
+    /**
+     *
+     * @return \f$ th_{Rd3} $\f
+     */
+    double getthRd3() const {
+        return thRd3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Rd1} $\f
+     */
+    double getphiRd1() const {
+        return phiRd1;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Rd2} $\f
+     */
+    double getphiRd2() const {
+        return phiRd2;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Rd3} $\f
+     */
+    double getphiRd3() const {
+        return phiRd3;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Rd4} $\f
+     */
+    double getphiRd4() const {
+        return phiRd4;
+    }
+    
+    /**
+     *
+     * @return \f$ phi_{Rd5} $\f
+     */
+    double getphiRd5() const {
+        return phiRd5;
+    }
+    
+    
+    ///////////////////
+    
+    
+    /**
+     *
+     * @return \f$ |CKM| $\f
+     */
+    gslpp::matrix<double> absCKM() const;
+    
+    /**
+     *
+     * @return \f$ \lambda_u $\f
+     */
+    gslpp::matrix<double> lambda_u() const;
+    
+    /**
+     *
+     * @return \f$ \lambda_d $\f
+     */
+    gslpp::matrix<double> lambda_d() const;
+    
+    /**
+     *
+     * @return \f$ V $\f
+     */
+    gslpp::matrix<double> create_V(double th1, double th2, double th3, double phi1, double phi2, double phi3, double phi4, double phi5) const;
+    
+    /**
+     *
+     * @return \f$ y_u $\f
+     */
+    gslpp::matrix<double> y_u() const;
+    
+    /**
+     *
+     * @return \f$ y_d $\f
+     */
+    gslpp::matrix<double> y_d() const;
+    
+    /**
+     *
+     * @return \f$ F_u $\f
+     */
+    gslpp::matrix<double> F_u() const;
+    
+    /**
+     *
+     * @return \f$ F_d $\f
+     */
+    gslpp::matrix<double> F_d() const;
     
 protected: 
     
@@ -69,8 +449,16 @@ protected:
 
 private:
     
-    double ma;
+    double eps, mtopEW;
+    double s12CKM, c12CKM, s13CKM, c13CKM, s23CKM, c23CKM, deltaCKM;
     
+    double QL1, QL2, QL3;
+    double QRu1, QRu2, QRu3;
+    double QRd1, QRd2, QRd3;
+    
+    double thL1, thL2, thL3, phiL1, phiL2, phiL3, phiL4, phiL5;
+    double thRu1, thRu2, thRu3, phiRu1, phiRu2, phiRu3, phiRu4, phiRu5;
+    double thRd1, thRd2, thRd3, phiRd1, phiRd2, phiRd3, phiRd4, phiRd5;
       
 };
 
